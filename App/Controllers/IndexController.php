72dcea41c356b15ee7877a4e90d524ac8ec44a4b
fsmorais2001 <mugiwara.tesouro@gmail.com>
2024-09-28 16:49:25 -0300

    Primeiro commit

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use MF\Controller\Action;
+
+class IndexController extends Action{
+
+    public function index(){
+
+        $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+        $this->render('index', 'layout1');
+
+    }
+
+    public function sobreNos(){
+        
+        $this->view->dados = array('Notebook', 'Smartphone');
+        $this->render('sobreNos','layout1');
+
+    }
+
+}
+
+?>
